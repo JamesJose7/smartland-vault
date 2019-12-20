2bfa320c8cfe126c50289f8b4c89a93ad9f6e957
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-12-20 17:44:45 -0500

    Added more properties to data containers

--- src/main/java/com/jeeps/smartlandvault/config/StartupConfig.java
+++ src/main/java/com/jeeps/smartlandvault/config/StartupConfig.java
@@ -1,7 +1,5 @@
 package com.jeeps.smartlandvault.config;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.jeeps.smartlandvault.nosql.data_container.DataContainer;
 import com.jeeps.smartlandvault.nosql.data_container.DataContainerRepository;
 import com.jeeps.smartlandvault.util.GenericJsonMapper;
@@ -10,8 +8,6 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Component
@@ -22,27 +18,35 @@ public class StartupConfig {
 
     @EventListener(ContextRefreshedEvent.class)
     public void bootConfiguration() {
-        Gson gson = new Gson();
-
         // Test object with data array
         Optional<DataContainer> arrayDataContainerOptional = dataContainerRepository.findById("test-container");
         if (arrayDataContainerOptional.isEmpty()) {
-            DataContainer arrayDataContainer = new DataContainer("test-container", "containing objects");
             String json = "[{\"screamId\":\"hRmUmDIyzjJ9339t8rVz\",\"body\":\"Hello world again\",\"userHandle\":\"user\",\"createdAt\":\"2019-12-20T17:27:25.854Z\"},{\"screamId\":\"ThVNlb1hjRlspATi1YA2\",\"body\":\"Hello world\",\"userHandle\":\"user\",\"createdAt\":\"2019-12-20T17:27:18.444Z\"}]";
-            arrayDataContainer.setData(GenericJsonMapper.convertFromJsonArray(json));
+            DataContainer arrayDataContainer = new DataContainer("test-container",
+                    "containing objects",
+                    "test",
+                    "http://example.org",
+                    true,
+                    GenericJsonMapper.convertFromJsonArray(json));
             dataContainerRepository.save(arrayDataContainer);
         }
 
         // Test object with single json element
         Optional<DataContainer> singleDataContainerOptional = dataContainerRepository.findById("single-container");
         if (singleDataContainerOptional.isEmpty()) {
-            DataContainer singleContainer = new DataContainer("single-container", "This one only has one object");
             String json = "{\"likeCount\":1,\"body\":\"Hello world again\",\"commentCount\":1,\"createdAt\":\"2019-12-20T17:27:25.854Z\",\"userHandle\":\"user\",\"userImage\":\"https://firebasestorage.googleapis.com/v0/b/socialape-8d0ee.appspot.com/o/7602692939.jpg?alt=media\",\"screamId\":\"hRmUmDIyzjJ9339t8rVz\",\"comments\":[{\"userImage\":\"https://firebasestorage.googleapis.com/v0/b/socialape-8d0ee.appspot.com/o/7602692939.jpg?alt=media\",\"screamId\":\"hRmUmDIyzjJ9339t8rVz\",\"body\":\"Don't overuse literally\",\"request\":\"user\",\"createdAt\":\"2019-12-20T17:27:54.009Z\"}]}";
-            singleContainer.setData(GenericJsonMapper.convertFromJsonArray(json));
+            DataContainer singleContainer = new DataContainer("single-container",
+                    "This one only has one object",
+                    "test",
+                    "http://example.org",
+                    false,
+                    GenericJsonMapper.convertFromJsonArray(json));
             dataContainerRepository.save(singleContainer);
         }
 
 
+        /*
+        Gson gson = new Gson();
         dataContainerRepository.findAll().forEach(container -> {
             if (container != null) {
                 if (container.getData() != null) {
@@ -62,6 +66,6 @@ public class StartupConfig {
 
                 }
             }
-        });
+        });*/
     }
 }
--- src/main/java/com/jeeps/smartlandvault/nosql/data_container/DataContainer.java
+++ src/main/java/com/jeeps/smartlandvault/nosql/data_container/DataContainer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Document
@@ -11,6 +12,11 @@ public class DataContainer {
     @Id
     private String id;
     private String name;
+    private String publisher;
+    private String sourceUrl;
+    private boolean isActive;
+    private int dataCount;
+    private int propertyCount;
     @JsonIgnore
     private List<Object> data;
 
@@ -19,6 +25,15 @@ public class DataContainer {
     public DataContainer(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public DataContainer(String id, String name, String publisher, String sourceUrl, boolean isActive, List<Object> data) {
+        this.id = id;
+        this.name = name;
+        this.publisher = publisher;
+        this.sourceUrl = sourceUrl;
+        this.isActive = isActive;
+        setData(data);
     }
 
     public String getId() {
@@ -37,12 +52,55 @@ public class DataContainer {
         this.name = name;
     }
 
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    public void setSourceUrl(String sourceUrl) {
+        this.sourceUrl = sourceUrl;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public int getDataCount() {
+        return dataCount;
+    }
+
+    public void setDataCount(int dataCount) {
+        this.dataCount = dataCount;
+    }
+
+    public int getPropertyCount() {
+        return propertyCount;
+    }
+
+    public void setPropertyCount(int propertyCount) {
+        this.propertyCount = propertyCount;
+    }
+
     public List<Object> getData() {
+        if (data == null) return new ArrayList<>();
         return data;
     }
 
     public void setData(List<Object> data) {
         this.data = data;
+        // update data count
+        setDataCount(data.size());
     }
 
     @Override

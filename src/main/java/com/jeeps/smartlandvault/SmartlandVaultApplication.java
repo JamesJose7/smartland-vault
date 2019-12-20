328d56e4b08f717e98595d6cb438d9430174d81e
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-12-20 11:36:26 -0500

    Initial commit

---
+++ src/main/java/com/jeeps/smartlandvault/SmartlandVaultApplication.java
@@ -0,0 +1,13 @@
+package com.jeeps.smartlandvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlandVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlandVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeeps/smartlandvault/SmartlandVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeeps.smartlandvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlandVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
